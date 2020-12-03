4626fa3d0fcb94a1fe33a5f304edfc641d779f33
fabio <ti@traca.com.br>
2020-12-03 20:59:24 -0300

    Permite rodar testes específicos e silenciar output

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,33 @@
-for f in tests/*
+
+
+while getopts ":o" o; do
+  case $o in
+    o)
+      output=1
+      ;;
+    *)
+      echo "invalid options";
+      exit 1;
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+if [ "$1" ]; then
+  list=("$1")
+else
+  list=(tests/*)
+fi
+
+for f in "${list[@]}"
 do
   code=`cat tester.c | sed -e "s@{TEST_FILE}@$f@g"`
   echo "$code" > tester_tmp.c
-  gcc tester_tmp.c && ./a.out
+  if [ -z $output ]; then
+    gcc tester_tmp.c && ./a.out > tester_output
+  else
+    gcc tester_tmp.c && ./a.out
+  fi
 done
 
